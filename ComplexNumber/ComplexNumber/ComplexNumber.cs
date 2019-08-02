bb5cf5b24f132caae0dfc00fb77da9e6343cc0dc
EvgenyVol <EvgenyB16@gmail.com>
2019-08-02 19:27:57 +0300

    add class ComplexNumber

---
+++ ComplexNumber/ComplexNumber/ComplexNumber.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplexNumber
+{
+    class ComplexNumber
+    {
+        float Re;
+        float Im;
+
+        public ComplexNumber(float re, float im)
+        {
+            Re = re;
+            Im = im;
+        }
+
+        public static void PrintComplexNumber(ComplexNumber number)
+        {
+            Console.WriteLine(number.Re + " + " + number.Im + "i");
+        }
+
+        public void AddComplexNumber(float re, float im)
+        {
+            Re += re;
+            Im += im;
+        }
+
+        public void SubtractComplexNumber(float re, float im)
+        {
+            Re -= re;
+            Im -= im;
+        }
+        public void MultiplyComplexNumber(float re, float im)
+        {
+            Re *= re;
+            Im *= im;
+        }
+    }
+}
---
+++ ComplexNumber/ComplexNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ComplexNumber;
+
+namespace ComplexNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("* * * Testing class complex number * * *");
+
+            ComplexNumber num = new ComplexNumber(5, 7);
+            ComplexNumber.PrintComplexNumber(num);
+            num.AddComplexNumber(2, 6);
+            ComplexNumber.PrintComplexNumber(num);
+            num.SubtractComplexNumber(1, 1);
+            ComplexNumber.PrintComplexNumber(num);
+            num.MultiplyComplexNumber(2, 2);
+            ComplexNumber.PrintComplexNumber(num);
+
+            Console.ReadKey();
+        }
+    }
+}
